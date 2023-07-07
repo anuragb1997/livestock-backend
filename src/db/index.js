a737fa4956856c7b68d3dca56dac5488552a55e5
anuragb1997 <anurag23.bhatt23@gmail.com>
2023-07-07 15:13:26 +0530

    use env for db config

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg')
+require("dotenv").config();
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'pern_auth',
-  password: 'Anurag@123',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 module.exports = {
