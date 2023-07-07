4c6b62bab7f1cd56df1000914e041f70dbe2a227
anuragb1997 <anurag23.bhatt23@gmail.com>
2023-07-07 14:41:30 +0530

    add api is woking for GET /

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ const authRoutes = require('./routes/auth')
 //initialize routes
 app.use('/api', authRoutes)
 
+app.get("/", (req, res) => {
+  return res.status(200).json("API IS WORKING");
+})
+
 //app start
 const appStart = () => {
   try {
