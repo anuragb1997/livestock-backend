7c5264eccba0f6cfe37be7823511d8fdf2071c06
anuragb1997 <anurag23.bhatt23@gmail.com>
2023-07-13 01:29:00 +0530

    expense

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -210,9 +210,10 @@ exports.sendExpense = async (req, res) => {
   try {
    
 
-    await db.query('insert into expenses(discription ,amount , user_id ) values ($1 , $2, $3 )', [
+    await db.query('insert into expenses(discription ,income ,expense, user_id ) values ($1 , $2, $3, $4 )', [
       discription ,
-      amount , 
+      income ,
+      expense, 
       user_id ,
     ])
 
